1ef7bd24590abfde157094e0c18e1d1a2d17c553
keiveulbugs <114590495+keiveulbugs@users.noreply.github.com>
2023-03-12 19:14:51 +0100

    make having a name provided optional

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use rust_embed::RustEmbed;
 struct Args {
    /// Name of the bot to create
    #[arg(short, long)]
-   name: String,
+   name: Option<String>,
 
 
    /// Deploy a basic command
@@ -38,6 +38,7 @@ pub struct Asset;
 fn main() {
    let args = Args::parse();
 
+   let botname = args.name.unwrap_or("".to_string());
 
    // Here starts the creation of a new library.
    println!("\nLet the Disco start 🎉!\n");
@@ -45,14 +46,14 @@ fn main() {
    // Runs ```cargo new``` to create a new directory.
 Command::new("cargo")
       .arg("new")
-      .arg(args.name.clone())
+      .arg(botname.clone())
       .status()
       .expect("Running cargo new failed");
 
-   println!("\nHi {}! Welcome to the party!\n", args.name);
+   println!("\nHi {}! Welcome to the party!\n", botname);
 
    // Update the Cargo.toml to include Poise and Tokio
-   let mut fileref = OpenOptions::new().append(true).open(format!(r"{}\Cargo.toml", args.name)).expect("Unable to open file");
+   let mut fileref = OpenOptions::new().append(true).open(format!(r"{}\Cargo.toml", botname)).expect("Unable to open file");
    fileref.write_all(
    r#"poise = { version = "0.4\5.2", features = ["cache"] }
 tokio = { version = "1", features = ["macros", "rt-multi-thread"] }
@@ -62,30 +63,30 @@ dotenv_codegen = "0.15.0"
    println!("Succesfully added all dependencies to Cargo.toml!");
 
    // Create a commands directory
-   fs::create_dir(format!(r"{}\src\commands", args.name)).expect("Failed creating the commands directory");
+   fs::create_dir(format!(r"{}\src\commands", botname)).expect("Failed creating the commands directory");
    println!("Succesfully created commands directory!");
 
 
    // Create mod.rs
-   let mut file = fs::File::create(format!(r"{}\src\commands\mod.rs", args.name)).expect("creating command file failed");
+   let mut file = fs::File::create(format!(r"{}\src\commands\mod.rs", botname)).expect("creating command file failed");
 
    // Fill main.rs
    let mainimport = Asset::get("def_com/main.rs").expect("Couldn't fetch main.rs file");
    let mainbytes = std::str::from_utf8(mainimport.data.as_ref()).expect("Couldn't convert main.rs to bytes");
-   let mut mainfile = OpenOptions::new().write(true).truncate(true).open(format!(r"{}\src\main.rs", args.name)).expect("Unable to open main.rs");
+   let mut mainfile = OpenOptions::new().write(true).truncate(true).open(format!(r"{}\src\main.rs", botname)).expect("Unable to open main.rs");
    mainfile.write_all(mainbytes.as_bytes()).expect("Couldn't write to main.rs");
    println!("Succesfully created the main.rs file!");
 
    // Create the help command
    let helpimport = Asset::get("def_com/help.rs").expect("Couldn't fetch help command file");
    let helpbytes = std::str::from_utf8(helpimport.data.as_ref()).expect("Couldn't convert help.rs to bytes");
-   let mut helpfile = fs::File::create(format!(r"{}\src\commands\help.rs", args.name)).expect("couldn't create help command file");
+   let mut helpfile = fs::File::create(format!(r"{}\src\commands\help.rs", botname)).expect("couldn't create help command file");
    helpfile.write_all(helpbytes.as_bytes()).expect("Couldn't create the help command");
    file.write_all(b"pub mod help;\n").expect("Failed to update mod");
    println!("Succesfully created the help command!");
 
    // Create .env file
-   let mut envfile = fs::File::create(format!(r"{}\.env", args.name)).expect("Couldn't create a .env file");
+   let mut envfile = fs::File::create(format!(r"{}\.env", botname)).expect("Couldn't create a .env file");
    envfile.write_all("DISCORD_TOKEN=".as_bytes()).expect("Couldn't write to .env file");
    println!("Succesfully added a .env file!");
 
@@ -94,14 +95,14 @@ dotenv_codegen = "0.15.0"
    if args.default.is_some() {
       commandtype = 0;
       let name = args.default.unwrap_or("template".to_string());
-      commands::setupcommand::setupcommand(name.clone(), args.name.clone(), commandtype);
+      commands::setupcommand::setupcommand(name.clone(), botname.clone(), commandtype);
       println!("Succesfully created a command with the name '{}'!", name);
    }
 
    if args.example.is_some() {
       commandtype = 1;
       let name = args.example.unwrap_or("template".to_string());
-      commands::setupcommand::setupcommand(name.clone(), args.name.clone(), commandtype);
+      commands::setupcommand::setupcommand(name.clone(), botname.clone(), commandtype);
       println!("Succesfully created a command with the name '{}'!", name);
    }
    
